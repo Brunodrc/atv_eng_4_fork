45ad3a7209ad86fcfa46db07894e4c58f951899a
Brunodrc <bruno.bdrc@gmail.com>
2023-03-21 23:57:12 -0300

    primeiro commit certo da aplicação, aleluia!

---
+++ store/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Category, Customer, Order, Products
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Products)
+
+# Username: oladjango
+# Email address: ola@gmail.com
+# Password: o123dj
---
+++ store/models/__init__.py
@@ -0,0 +1,4 @@
+from .products import Products
+from .category import Category
+from  .custumer import  Customer
+from  .orders import  Order
